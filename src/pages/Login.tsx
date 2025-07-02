19d1bf483c0fdba4fec4419f1d94484f7c35c2f9
densnap <shivam.sanap@dentsu.com>
2025-07-02 17:40:47 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -31,7 +31,7 @@ const Login = () => {
       return;
     }
    try {
-      const response = await fetch("http://127.0.0.1:9001/api/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
